a40bbe61c9184a2ca3f7f992068b9cf8a5beff51
YukiHirofusa <snow39lespaul@gmail.com>
2018-11-02 02:32:38 +0900

    画面共有beta

--- taiga_script.js
+++ taiga_script.js
@@ -130,3 +130,37 @@ $(function () {
     $('#specificationCont').find('button').hide();
   });
 });
+
+
+//ss機能
+
+const ss = ScreenShare.create({debug: true});
+
+$(function(){
+    $('#start-screen').on('click', () => {
+        if (ss.isScreenShareAvailable() === false) {
+          alert('Screen Share cannot be used. Please install the Chrome extension.');
+          return;
+        }
+    
+        ss.start({
+          width:     $('#Width').val(),
+          height:    $('#Height').val(),
+          frameRate: $('#FrameRate').val(),
+        })
+          .then(stream => {
+            $('#my-video')[0].srcObject = stream;
+    
+            if (existingCall !== null) {
+              const peerid = existingCall.peer;
+              existingCall.close();
+              const call = peer.call(peerid, stream);
+              step3(call);
+            }
+            localStream = stream;
+          })
+          .catch(error => {
+              console.log(error);
+          });
+      });
+});
